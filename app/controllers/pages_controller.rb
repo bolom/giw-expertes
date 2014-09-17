f2b73bc41d1fb0369e02c98be8b4f8f488ec9353
AlSquire <alsquire@gmail.com>
2014-09-17 14:21:05 +0200

    Add profiles#show and recent profiles in pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,5 @@
 class PagesController < ApplicationController
+  def home
+    @profiles = Profile.active.order(:created_at).last(3)
+  end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,8 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.active
   end
+
+  def show
+    @profile = Profile.active.find(params[:id])
+  end
 end
