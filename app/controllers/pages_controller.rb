b931a8e685d6252e924509243d62118c7452ad29
AlSquire <alsquire@gmail.com>
2014-10-06 20:32:03 +0200

    Four random profiles, not three

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @random_profiles = Profile.active.order_by_rand.limit(3)
+    @random_profiles = Profile.active.order_by_rand.limit(4)
   end
 end
