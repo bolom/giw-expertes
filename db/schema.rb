7e5448707d049b9c7e8e05c9f5ab9cf1604c63cf
AlSquire <alsquire@gmail.com>
2014-10-06 18:24:36 +0200

    Add Skill and Domain with seed data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917105320) do
+ActiveRecord::Schema.define(version: 20141006155015) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -46,11 +46,24 @@ ActiveRecord::Schema.define(version: 20140917105320) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "domains", force: true do |t|
+    t.string   "label"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "profiles", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.boolean  "active"
     t.text     "linked_in_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: true do |t|
+    t.string   "label"
+    t.integer  "domain_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,109 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+{
+  "Gestion de projets et méthodologies" => [
+    "Design Thinking",
+    "DevOps",
+    "Lean Startup",
+    "Méthode Agile",
+    "Product Owner",
+    "Scrum Master"
+  ],
+
+  "Développement" => [
+    "Android",
+    "C++",
+    "HTML5",
+    "iOS",
+    "Java",
+    "Javascript",
+    "Linux",
+    "Open Source",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Symfony",
+    ".NET"
+  ],
+
+  "Communication, marketing et contenu" => [
+    "Community management",
+    "Contenu éditorial",
+    "Cross-canal",
+    "Cross-media",
+    "Cross-platform",
+    "Digital PR",
+    "Digital Storytelling",
+    "Display",
+    "e-CRM",
+    "Loyalty",
+    "Medias",
+    "Planning stratégique",
+    "Régie mobile",
+    "Retargeting",
+    "SEO",
+    "SMO",
+    "Social Media",
+    "Social TV"
+  ],
+
+  "Innovation, design et nouvelles technologies" => [
+    "Big Data",
+    "Cloud Computing",
+    "Data Science",
+    "e-Santé",
+    "e-Education",
+    "Ergonomie",
+    "Formation/enseignement au numérique",
+    "Gaming",
+    "IHM",
+    "Low Tech",
+    "Management de l'innovation",
+    "Objets connectés",
+    "Open Data",
+    "Open Innovation",
+    "Robotique",
+    "Sharing Economy",
+    "UI Design",
+    "UX Design",
+    "Wearable Products",
+    "Web Design"
+  ],
+
+  "Politique, business et investissement" => [
+    "Business Model",
+    "Crowdfunding",
+    "E-Commerce",
+    "Entrepreneuriat ",
+    "Juridique",
+    "M-Commerce",
+    "Mobile",
+    "Monétisation",
+    "Pitch",
+    "Start-up",
+    "Stratégie",
+    "Venture Capitalism",
+    "Web-to-Store",
+    "Réglementation et législation",
+    "Politique"
+  ],
+
+  "Technique, Administration réseau, système et base de données" => [
+    "Câbles",
+    "DBA",
+    "DNS",
+    "Hacker",
+    "Matériel et Data Center",
+    "Normalisation",
+    "Routage",
+    "Sécurité",
+    "Unix",
+    "Windows"
+  ]
+}.each do |domain_label, skill_labels|
+  domain = Domain.create(label: domain_label)
+  skill_labels.each do |skill_label|
+    domain.skills.create(label: skill_label)
+  end
+end
