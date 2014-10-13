db54bb7e7857937229c11588345336d17f189d63
Jérémie Berduck <jeremie@omts.fr>
2014-10-13 11:39:09 +0200

    Remove search filter on mobile...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,7 +71,7 @@ $(function(){
   			}
   		}
   });
-    $('.searchform').keydown(function(event){
+    $('.searchform input').keydown(function(event){
     if(event.keyCode == 13) {
       event.preventDefault();
       return false;
