daea97dff308e2c6c34472caf50e59e64e4f6fe1
Jérémie Berduck <jeremie@omts.fr>
2014-10-13 18:38:04 +0200

    Form fixes & Mobile fixes
    \o/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap-select
+//= require defaults-fr_FR
 //= require jasny-bootstrap.min
 //= require jquery.flexslider-min
 //= require jquery.slicknav.min
